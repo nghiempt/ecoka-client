a0b4bf023120bccedddd15b9269a0dbf374d4a80
nhilt160703 <nhilt@kdigi.vn>
2024-11-20 12:56:23 +0700

    fix: home and product

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -35,7 +35,8 @@ interface ESG {
 }
 
 export function HomePage() {
-    const [products, setProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<{ [key: string]: Product[] }>({});
+
     const [loading, setLoading] = useState<boolean>(true);
     const [esgs, setEsgs] = useState<ESG[]>([]);
 
@@ -45,45 +46,74 @@ export function HomePage() {
             myHeaders.append("Content-Type", "application/json");
 
             const raw = JSON.stringify({
-                method: "GET"
+                method: "GET",
             });
 
             const requestOptions = {
                 method: "POST",
                 headers: myHeaders,
                 body: raw,
-                redirect: "follow" as RequestRedirect
+                redirect: "follow" as RequestRedirect,
             };
 
-            const res = await fetch("https://n8n.khiemfle.com/webhook/5c404ea1-4a57-4c0a-8628-3088d00abe64", requestOptions);
+            const res = await fetch(
+                "https://n8n.khiemfle.com/webhook/5c404ea1-4a57-4c0a-8628-3088d00abe64",
+                requestOptions
+            );
+
             if (!res.ok) {
-                throw new Error('Failed to fetch data');
+                throw new Error("Failed to fetch data");
             }
+
             const data = await res.json();
-            console.log(data);
-            const transformedProducts: Product[] = data.map((item: any) => ({
-                row: item.row_number,
-                id: item.id,
-                name: item.name,
-                category: item.category,
-                price: item.price,
-                description: item.description,
-                images: [
-                    item.i_one,
-                    item.i_two,
-                    item.i_three,
-                    item.i_four,
-                    item.i_five,
-                    item.i_six,
-                ].filter((url) => url !== ""),
-            }));
-            setProducts(transformedProducts.sort((a, b) => b.id - a.id).slice(0, 8));
+            const groupedProducts: { [key: string]: Product[] } = {};
+            data.forEach((item: any) => {
+                const category = item.category;
+                if (!groupedProducts[category]) {
+                    groupedProducts[category] = [];
+                }
+                groupedProducts[category].push({
+                    row: item.row_number,
+                    id: item.id,
+                    name: item.name,
+                    category: item.category,
+                    price: item.price,
+                    description: item.description,
+                    images: [
+                        item.i_one,
+                        item.i_two,
+                        item.i_three,
+                        item.i_four,
+                        item.i_five,
+                        item.i_six,
+                    ].filter((url) => url !== ""),
+                });
+            });
+
+            Object.keys(groupedProducts).forEach((category) => {
+                groupedProducts[category] = groupedProducts[category]
+                    .sort((a, b) => b.id - a.id)
+                    .slice(0, 4);
+            });
+
+            const filteredCategories = Object.keys(groupedProducts)
+                .filter((category) => groupedProducts[category].length >= 4);
+
+            const limitedCategories = filteredCategories.slice(0, 4);
+
+            const limitedProducts: { [key: string]: Product[] } = {};
+            limitedCategories.forEach((category) => {
+                limitedProducts[category] = groupedProducts[category];
+            });
+
+            setProducts(limitedProducts);
         } catch (err) {
             console.log(err);
         } finally {
             setLoading(false);
         }
     };
+
 
     const fetchEsgs = async () => {
         try {
@@ -127,7 +157,7 @@ export function HomePage() {
                 <video className="w-full h-[680px] md:h-[1000px] lg:h-[1000px] object-cover" autoPlay loop muted>
                     <source src={IMAGES.VIDEO_HOME} type="video/mp4" />
                 </video>
-                <div className='w-full absolute top-0 pt-6 flex flex-col justify-center items-center gap-10 text-white uppercase'>
+                <div className='w-full absolute top-0 pt-6 flex flex-col justify-center items-center gap-10 text-white'>
                     <nav className="w-full hidden lg:flex flex-row justify-center items-center gap-4 py-6">
                         <Link href={ROUTES.HOME} className="bg-gray-50 bg-opacity-60 text-[20px] text-[rgb(var(--quaternary-rgb))] font-bold px-4 py-2 rounded-lg h-full flex items-center justify-center hover:bg-[rgb(var(--quaternary-rgb))] hover:opacity-70 hover:text-white">Trang Chủ</Link>
 
@@ -174,7 +204,7 @@ export function HomePage() {
                                 <div
                                     className="flex flex-col justify-center items-center gap-5 transform transition-transform hover:scale-110 hover:cursor-pointer"
                                 >
-                                    <div className="font-semibold text-xl lg:text-2xl md:text-2xl text-center">
+                                    <div className="font-bold text-xl lg:text-2xl md:text-2xl text-center">
                                         {esg.title}
                                     </div>
                                     <div
@@ -195,6 +225,29 @@ export function HomePage() {
                     </div>
                 </div>
                 <div className="w-full h-1 bg-[rgb(var(--primary-rgb))]"></div>
+                {/* <div className="w-full flex flex-col justify-start items-center mb-14 mt-8">
+                    <div className="text-3xl font-bold text-center mb-8">Sản Phẩm</div>
+                    <div className="w-full mb-8">
+                        {loading ? (
+                            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                                {Array.from({ length: 8 }).map((_, index) => (
+                                    <div key={index} className="animate-pulse flex flex-col items-center">
+                                        <div className="w-32 h-32 bg-gray-300 rounded-md mb-4"></div>
+                                        <div className="w-24 h-4 bg-gray-300 rounded-md mb-2"></div>
+                                        <div className="w-16 h-4 bg-gray-300 rounded-md"></div>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <Product products={Object.values(products).flat()} />
+                        )}
+                    </div>
+                    <Link className="w-full flex justify-center items-center" href={ROUTES.PRODUCT}>
+                        <Button className="w-full md:w-1/5 lg:w-1/5 rounded-sm bg-white border border-[rgb(var(--primary-rgb))] text-[rgb(var(--primary-rgb))] font-bold hover:bg-[rgb(var(--primary-rgb))] hover:text-white truncate">
+                            Xem thêm
+                        </Button>
+                    </Link>
+                </div> */}
                 <div className="w-full flex flex-col justify-start items-center mb-14 mt-8">
                     <div className="text-3xl font-bold text-center mb-8">Sản Phẩm</div>
                     <div className="w-full mb-8">
@@ -209,7 +262,42 @@ export function HomePage() {
                                 ))}
                             </div>
                         ) : (
-                            <Product products={products} />
+                            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                                {Object.entries(products)
+                                    .filter(([category, items]) => items.length >= 4)
+                                    .flatMap(([category, items]) =>
+                                        items.map((product) => (
+                                            <div key={product.id} className="relative group cursor-pointer rounded-lg">
+                                                <div className="rounded-lg bg-gray-50 flex flex-col border-none">
+                                                    <div className="relative w-full h-[280px] rounded-lg">
+                                                        <Image
+                                                            src={product.images[0]}
+                                                            alt={`${product.name} image`}
+                                                            fill
+                                                            style={{ objectFit: "cover" }}
+                                                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                                            className="rounded-lg"
+                                                        />
+                                                    </div>
+                                                    <div className="flex flex-col justify-center p-3 text-start">
+                                                        <div className="text-lg font-bold mb-1 max-h-[28px] truncate">{product.name}</div>
+                                                        <div className="text-xs font-semibold text-gray-400 text-left mb-2 max-h-[32px] text-clip overflow-hidden">
+                                                            {product.description}
+                                                        </div>
+                                                        <div className="w-full grid grid-cols-5 items-center">
+                                                            <p className="col-span-3 max-h-[24px] text-md font-semibold text-left truncate">
+                                                                {Intl.NumberFormat("de-DE").format(product.price)} VND
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className="absolute top-2 right-2 h-10 w-10 rounded-full bg-[#E97171] text-white text-[12px] font-semibold flex items-center justify-center">
+                                                    New
+                                                </div>
+                                            </div>
+                                        ))
+                                    )}
+                            </div>
                         )}
                     </div>
                     <Link className="w-full flex justify-center items-center" href={ROUTES.PRODUCT}>
@@ -218,6 +306,7 @@ export function HomePage() {
                         </Button>
                     </Link>
                 </div>
+
                 <div className="w-full py-10 bg-[rgb(var(--secondary-rgb))] px-10 lg:lx-0 md:px-0 mb-24 rounded-lg">
                     <Slider />
                 </div>
--- src/modules/product/index.tsx
+++ src/modules/product/index.tsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { Product } from "@/components/global/product"
-import { Footer } from "@/layout/footer"
-import { Header } from "@/layout/header"
-import { NavMobile } from "@/layout/nav-mobile"
-import { IMAGES } from "@/utils/image"
-import { ROUTES } from "@/utils/route"
-import { ChevronRight } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
-import { useEffect, useState } from "react"
+import { Product } from "@/components/global/product";
+import { Footer } from "@/layout/footer";
+import { Header } from "@/layout/header";
+import { NavMobile } from "@/layout/nav-mobile";
+import { IMAGES } from "@/utils/image";
+import { ROUTES } from "@/utils/route";
+import { ChevronRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 interface Product {
     row: number;
@@ -19,10 +19,13 @@ interface Product {
     price: number;
     description: string;
     images: string[];
-};
+}
 
 export function ProductPage() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+    const [categories, setCategories] = useState<string[]>([]);
+    const [selectedCategory, setSelectedCategory] = useState<string>("all");
     const [loading, setLoading] = useState<boolean>(true);
 
     const fetchProducts = async () => {
@@ -31,22 +34,21 @@ export function ProductPage() {
             myHeaders.append("Content-Type", "application/json");
 
             const raw = JSON.stringify({
-                method: "GET"
+                method: "GET",
             });
 
             const requestOptions = {
                 method: "POST",
                 headers: myHeaders,
                 body: raw,
-                redirect: "follow" as RequestRedirect
+                redirect: "follow" as RequestRedirect,
             };
 
             const res = await fetch("https://n8n.khiemfle.com/webhook/5c404ea1-4a57-4c0a-8628-3088d00abe64", requestOptions);
             if (!res.ok) {
-                throw new Error('Failed to fetch data');
+                throw new Error("Failed to fetch data");
             }
             const data = await res.json();
-            console.log(data);
             const transformedProducts: Product[] = data.map((item: any) => ({
                 row: item.row_number,
                 id: item.id,
@@ -64,6 +66,10 @@ export function ProductPage() {
                 ].filter((url) => url !== ""),
             }));
             setProducts(transformedProducts.sort((a, b) => b.id - a.id));
+
+            // Extract unique categories
+            const uniqueCategories = Array.from(new Set(transformedProducts.map((p) => p.category)));
+            setCategories(uniqueCategories);
         } catch (err) {
             console.log(err);
         } finally {
@@ -71,27 +77,44 @@ export function ProductPage() {
         }
     };
 
+    const handleFilterChange = (category: string) => {
+        setSelectedCategory(category);
+        if (category === "all") {
+            setFilteredProducts(products);
+        } else {
+            setFilteredProducts(products.filter((product) => product.category === category));
+        }
+    };
+
     useEffect(() => {
         fetchProducts();
     }, []);
+
+    useEffect(() => {
+        if (selectedCategory === "all") {
+            setFilteredProducts(products);
+        } else {
+            setFilteredProducts(products.filter((product) => product.category === selectedCategory));
+        }
+    }, [selectedCategory, products]);
 
     return (
         <div className="w-full min-h-screen flex flex-col justify-start items-center relative">
             <Header />
             <NavMobile />
-            <div className="bg-cover bg-center h-[250px] w-full md:w-2/3 lg:w-2/3 flex justify-center items-center md:rounded-lg lg:rounded-lg"
-                style={{ backgroundImage: `url('/breadcrumb.png')` }}>
+            <div
+                className="bg-cover bg-center h-[250px] w-full md:w-2/3 lg:w-2/3 flex justify-center items-center md:rounded-lg lg:rounded-lg"
+                style={{ backgroundImage: `url('/breadcrumb.png')` }}
+            >
                 <div className="w-full flex flex-col justify-center items-center">
                     <Image
                         src={IMAGES.BANNER_LOGO}
-                        alt='img'
+                        alt="img"
                         width={50}
                         height={50}
                         className="text-center"
                     />
-                    <h1 className="text-4xl font-semibold mb-2">
-                        SẢN PHẨM
-                    </h1>
+                    <h1 className="text-4xl font-semibold mb-2">SẢN PHẨM</h1>
                     <div className="flex gap-2 items-center">
                         <Link href={ROUTES.HOME} className="font-semibold text-sm">
                             Trang chủ
@@ -101,9 +124,31 @@ export function ProductPage() {
                     </div>
                 </div>
             </div>
-            <div className="w-5/6 md:w-2/3 lg:w-2/3 flex flex-col justify-center items-center">
-                <div className="w-full h-10 rounded-md bg-[rgb(var(--primary-rgb))] opacity-30 my-6"></div>
-                <div className="w-full">
+            <div className="w-5/6 md:w-2/3 lg:w-2/3 flex flex-col lg:flex-row mt-8 gap-8">
+                <div className="w-full lg:w-1/5 rounded-lg mb-6 lg:mb-0">
+                    <h3 className="text-lg font-semibold mb-4">Lọc theo danh mục</h3>
+                    <ul className="space-y-3">
+                        <li
+                            className={`cursor-pointer py-2.5 px-4 rounded-md ${selectedCategory === "all" ? " bg-white text-gray-950 shadow-lg font-medium" : "text-black"
+                                }`}
+                            onClick={() => handleFilterChange("all")}
+                        >
+                            Tất Cả Sản Phẩm
+                        </li>
+                        {categories.map((category) => (
+                            <li
+                                key={category}
+                                className={`cursor-pointer py-2.5 px-4 rounded-md ${selectedCategory === category ? " bg-white text-gray-950 shadow-lg font-medium " : "text-black"
+                                    }`}
+                                onClick={() => handleFilterChange(category)}
+                            >
+                                {category}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="w-full lg:w-4/5">
                     {loading ? (
                         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                             {Array.from({ length: 8 }).map((_, index) => (
@@ -115,12 +160,12 @@ export function ProductPage() {
                             ))}
                         </div>
                     ) : (
-                        <Product products={products} />
+                        <Product products={filteredProducts} />
                     )}
                 </div>
             </div>
             <div className="w-5/6 md:w-2/3 lg:w-2/3 h-[3px] bg-[rgb(var(--quaternary-rgb))] my-10"></div>
             <Footer />
         </div>
-    )
+    );
 }
