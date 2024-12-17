50ab674b71f539b48591fcd056715683f57f8ac9
nchieune <hieuncce180986@fpt.edu.vn>
2024-12-17 16:01:04 +0700

    update

--- src/i18n/get-dictionary.ts
+++ src/i18n/get-dictionary.ts
@@ -1,10 +1,10 @@
-import type { Locale } from './settings'
+import type { Locale } from "./settings";
 
 const dictionaries: any = {
-    en: () => import('./dictionaries/en.json').then(module => module.default),
-    vi: () => import('./dictionaries/vi.json').then(module => module.default),
-    jp: () => import('./dictionaries/jp.json').then(module => module.default),
-}
+  en: () => import("./dictionaries/en.json").then((module) => module.default),
+  vi: () => import("./dictionaries/vi.json").then((module) => module.default),
+  jp: () => import("./dictionaries/jp.json").then((module) => module.default),
+};
 
 export const getDictionary = async (locale: Locale) =>
-    dictionaries[locale]?.() ?? dictionaries.vi()
+  dictionaries[locale]?.() ?? dictionaries.vi();
